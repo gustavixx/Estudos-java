7dd0b2768c5182ac103e716f53792b7252fd814d
gustavixx <gustavo230906@gmail.com>
2026-08-09 15:51:11 -0300

    math
    math

--- math.java
+++ math.java
@@ -2,34 +2,31 @@
 public class math {
 
 	public static void main(String[] args) {
-	
+
 		double x = 3.0;
 		double y = 4.0;
 		double z = -5.0;
 		double A, B, C;
-		
+
+		Math.sqrt(x);
 		A = Math.sqrt(x);
 		B = Math.sqrt(y);
 		C = Math.sqrt(25.0);
 		System.out.println("Raiz quadrada de" + x + "=" + A);
 		System.out.println("Raiz quadrade de" + x + "=" + B);
 		System.out.println("Raiz quadrade de 25 =" + C);
-		
-		A =Math.pow(x, z);
-		B =Math.pow(x,  2.0);
-		C =Math.pow(5.0,  2.0);
-		System.out.println(x +" elevado a "+y+"="+A);
-		System.out.println(x+"elevado ao quadrado = "+B);
-		System.out.println("5 elevado ao quadradp = " +C);
-		
-		A= Math.abs(y);
-		B= Math.abs(z);
-	System.out.println("Valor absoluto de "+ y + " = "+A);
-	System.out.println("Valor absoluto de " + z + " = " +B);
-		
-		
-		
-		
+
+		A = Math.pow(x, z);
+		B = Math.pow(x, 2.0);
+		C = Math.pow(5.0, 2.0);
+		System.out.println(x + " elevado a " + y + "=" + A);
+		System.out.println(x + "elevado ao quadrado = " + B);
+		System.out.println("5 elevado ao quadradp = " + C);
+
+		A = Math.abs(y);
+		B = Math.abs(z);
+		System.out.println("Valor absoluto de " + y + " = " + A);
+		System.out.println("Valor absoluto de " + z + " = " + B);
 
 	}
 
