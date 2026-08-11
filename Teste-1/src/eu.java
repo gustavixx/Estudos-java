99669e37eecf631683970dbdab8d678ec67817f0
gustavixx <gustavo230906@gmail.com>
2026-08-11 13:55:53 -0300

    Adiciona atividade Vetores

---
+++ Teste-1/src/eu.java
@@ -0,0 +1,8 @@
+
+public class eu {
+
+	public static void main(String[] args) {
+		
+		
+
+}
---
+++ Teste-1/teste.java
@@ -0,0 +1,14 @@
+package eu;
+
+public class eu {
+	
+	public static void main(String[]args) {
+		
+	int a = 7;
+	int b = 7;
+	int soma = a + b;
+	
+	System.out.print("O resultado e : "+ soma);
+	}
+
+}
