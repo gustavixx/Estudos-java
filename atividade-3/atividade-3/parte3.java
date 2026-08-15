09f7aa718c9d2ee2e7b2666ed4ec9882d894f9d0
gustavixx <gustavo230906@gmail.com>
2026-08-15 16:34:13 -0300

    Adiciona codigos das atividades 3 e 4

---
+++ atividade-3/atividade-3/parte3.java
@@ -0,0 +1,18 @@
+
+public class parte3 {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		
+		resultado = a / b;
+		
+		System.out.println(resultado);
+
+	}
+
+}
